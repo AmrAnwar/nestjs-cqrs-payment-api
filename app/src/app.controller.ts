d2e6b2af9ede828547f02512012e698676767370
Amr Anwar <amranwar945@gmail.com>
2022-09-14 21:08:31 +0200

    refactor: fix prettier issues

--- app/src/app.controller.ts
+++ app/src/app.controller.ts
@@ -1,17 +1,16 @@
-import {Controller, Get, Param, ParseIntPipe} from '@nestjs/common';
-import {Repository} from 'typeorm';
-import {AppService} from './app.service';
-import {OrganisationEntity} from './db/entitys/organisation.entity';
-import {InjectRepository} from '@nestjs/typeorm';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { AppService } from './app.service';
+import { OrganisationEntity } from './db/entitys/organisation.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Controller()
 export class AppController {
   constructor(
     private readonly appService: AppService,
     @InjectRepository(OrganisationEntity)
-    private readonly organisations: Repository<OrganisationEntity>
-  ) {
-  }
+    private readonly organisations: Repository<OrganisationEntity>,
+  ) {}
 
   @Get()
   getHello(): string {
@@ -20,8 +19,6 @@ export class AppController {
 
   @Get('/organisation/:id')
   async getOrganisation(@Param('id', ParseIntPipe) id: number) {
-    return this.organisations.findOne({where: {organisationId: id}});
+    return this.organisations.findOne({ where: { organisationId: id } });
   }
 }
-
-
